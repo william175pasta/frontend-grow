fa005f4bf6b29b74552cbe4162882074b05e9f5a
jerrytang <jerrymaxm@gmail.com>
2019-11-02 00:41:25 +0800

    調整第四天作業new array寫法

--- day4/index4.js
+++ day4/index4.js
@@ -1,24 +1,18 @@
 //   班級
-var classList = new Array();
+var classList = [];
 
 for (var c = 0; c < 10; c++) {
-    xx = c + 1;
-    classList[c] = `第${xx}班`;
-    console.log(`第${xx}班`);
+    classList.push(`第${c+1}班`);
 }
-console.log(classList);
+console.log('班級清單:', classList);
 
 //   學生
-var studentList = new Array();
+var studentList = [];
 
-for (var i = 0; i < 30; i++) {
-    x = i + 1;
-    studentList[i] = `學生${x}號`;
-    console.log(`學生${x}號`);
-
+for (var s = 0; s < 30; s++) {
+    studentList.push(`學生${s+1}號`);
 }
-console.log(studentList);
-
+console.log('學生清單:', studentList);
 
 //判斷型態
 var class1 = typeof (classList);
